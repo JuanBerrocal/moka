8f18d0b73d206c05bf95362d630bdfa1808a2317
JuanBerrocal <juan.brudner@gmail.com>
2026-07-05 09:12:39 +0200

    Some extra validations for UpdateStoreDto

--- Moka/DTOs/UpdateStoreDto.cs
+++ Moka/DTOs/UpdateStoreDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Moka.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moka.DTOs
 {
     public class UpdateStoreDto
     {
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
+        [StringLength(20)]
         public string? SapCode { get; set; }
     }
 }
