5647caf23fcd907c29bc76127aaa6afdd374e073
JuanBerrocal <juan.brudner@gmail.com>
2026-09-09 20:51:27 +0200

    form error message displayed when operation fails

--- moka-web/src/scenes/stores/StoreDetailScene.tsx
+++ moka-web/src/scenes/stores/StoreDetailScene.tsx
@@ -7,6 +7,7 @@ import {createStore} from "@/features/stores/api/storesApi";
 export const StoreDetailScene = () => {
 
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const [error, setError] = useState<string | null>(null);
     const navigate = useNavigate();
     const initialValues: StoreFormData = {
         name: "",
@@ -22,12 +23,14 @@ export const StoreDetailScene = () => {
     const onSubmit = async (values: StoreFormData) => {
         try {
             setIsSubmitting(true);
+            setError(null);
             const newStore = await createStore(values);
             console.log("Store created", newStore);
             navigate("/stores", {state: {message: "Store created successfully!"}});
         }
         catch (error) {
             console.error("Error creating store:", error);
+            setError("Error creating the store");
         }
         finally {
             setIsSubmitting(false);
@@ -38,6 +41,7 @@ export const StoreDetailScene = () => {
         <div>
             <h3>Store Detail</h3>
             <Link to="/stores">Back to Stores</Link>
+            { error &&  <div>{error}</div> }
             <StoreForm initialValues = {initialValues} onSubmit = {onSubmit} isSubmitting = {isSubmitting}/>
         </div>
     )
